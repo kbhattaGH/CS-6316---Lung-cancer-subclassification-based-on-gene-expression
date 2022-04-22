7d699195a4dd10ca80b24e4100d59033ee67a41b
Kshitij <bhattakshitij@gmail.com>
2022-04-22 13:40:04 -0400

    another pca code

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 ##
 import numpy as np
 import sklearn as sk
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import sys
+import os
 
-
+#
 class CancerClassifier:
-    def __init__(self, data, gamma, split):
-        self.data = data
-        self.Train = split[0]
-        self.Test = split[1]
+    def __init__(self, gamma, data, components=1000):
         self.gamma = gamma
+        self.components = components
+        self.data = data
 
-    def process_data(self):
-        #import data from processed file and extract required meta-data and train-tes split
-        X_Train = self.data[0:int(self.Train/100*len(self.data)), 0]
-        Y_Train = self.data[0:int(self.Train/100*len(self.data)), 1]
-        X_Test = self.data[len(X_Train):len(self.data), 0]
-        Y_Test = self.data[len(Y_Train):len(self.data), 1]
-        return X_Train, Y_Train, X_Test, Y_Test
+    # def process_data(self):
+    #     #import data from processed file and extract required meta-data and train-tes split
+    #     features = self.data.drop(data.drop(columns=['label','index']))
+    #     labels = self.data[['index','label']]
+    #     SS = StandardScaler(copy=True, with_mean=True, with_std=True)
+    #     SS.fit(features)
+    #     scaled_features = SS.transform(features)
+    #     pca = PCA(n_components=self.components)
+    #     trans_data = pca.fit_transform(scaled_features)  # need to check variance ot make sure
+    #
+    #     X_Train, X_Test, Y_Train, Y_Test = sk.model_selection.train_test_split()
+    #
+    #     return X_Train, Y_Train, X_Test, Y_Test
 
-    def train_classifier(self, X_Train, Y_Train):
+
+
+    def train_classifier(self):
+
         cls = sk.svm.SVC(gamma=self.gamma)
         cls.fit(X_Train, Y_Train)
         train_score = cls.score(X_Train, Y_Train)
@@ -27,14 +40,19 @@ class CancerClassifier:
     def test_and_graph_results(self, cls, X_Test, Y_Test):
         X_predicted = cls.predict(X_Test)
         X_predicted_score = cls.score(X_predicted, Y_Test)
+# #
 
-
-#if __name__ == "__main__":
-    #filename = ""
-    #with open(filename,'r'):
-        #f1= ...
+if __name__ == "__main__":
+    abspath = sys.path[0]
+    data_path = os.path.join(abspath, 'data')
+    features = pd.read_csv(data_path+'/demo_feature_file.csv', sep=',', index_col=0)
+    labels = pd.read_csv(data_path+'/demo.csv', sep=',', index_col=0)
+    data = (pd.concat([features, labels], axis=1)).reset_index()
         #cfier = CancerClassifier(f1, gamma=50, split=[80,20])
         #data = cfier.process_data()
+    print(data.keys())
+    print (data[['index','label']])
+
 
 
 
